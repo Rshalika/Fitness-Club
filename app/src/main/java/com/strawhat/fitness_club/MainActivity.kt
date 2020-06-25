d1331eb81fe617d329d17e4e96d92f7ba75d326a
Eobard <r.shalika08@gmail.com>
2020-06-25 18:27:53 +0400

    create header design

--- app/src/main/java/com/strawhat/fitness_club/MainActivity.kt
+++ app/src/main/java/com/strawhat/fitness_club/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.strawhat.fitness_club
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.CollapsingToolbarLayout
 
@@ -16,15 +14,4 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
