83d5224d26a7c1b873b0963b8f9192ebc895219b
Eobard <r.shalika08@gmail.com>
2020-06-25 15:15:36 +0400

    clean up imports

--- app/src/main/java/com/strawhat/fitness_club/MainActivity.kt
+++ app/src/main/java/com/strawhat/fitness_club/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.strawhat.fitness_club
 
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class MainActivity : AppCompatActivity() {
 
